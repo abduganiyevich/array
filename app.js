0d4569f55791a920be9b0cb318fc3ed78ed91827
abduganiyevich <abduganiyevyahyobek555@gmail.com>
2023-10-20 20:10:49 +0500

    masala

---
+++ app.js
@@ -0,0 +1,48 @@
+// 1) ndan m gacha nollar soni
+
+////////////////////////////////////////////////////////////
+
+// 2) ikkita massiv yozing qaysi massiv katta bolsa shuni massiv oldin yozilsin
+
+// function maxArr() {
+//     let arr=[1,2,3,4,5,6,7,8,9];
+//     let arr2=["salom","bu","shunchaki","array"];
+//     if (arr.length>arr2.length) {
+//        return arr.concat(arr2);
+//     }
+//     else{
+//         return arr2.concat(arr);
+//     }
+   
+// }
+// console.log(maxArr());
+
+////////////////////////////////////////////////////////////////
+
+// 3) sezar
+
+
+
+///////////////////////////////////////////////////////////////
+
+// 4) massivga teskari massive yasash;
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+/////////////////////////////////////////////////////////////////
+
+// 5) berilgan massivgacha o'chirish
+
+// let a =+prompt("nechta ma'lumotni o'chirmoqchisiz");
+// function erase() {
+//     let arr=[1,2,3,4,5,6,7,8,9];
+//     return arr.splice(a);
+// }
+// console.log(erase(a));
+
+//////////////////////////////////////////////////////////////////
+
+  
